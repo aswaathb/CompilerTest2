85c66aeedd8fa9b4cf6e0b7cc991cd239f25a865
Aswaath Balasubramanian <aswaathb@gmail.com>
2018-03-29 16:53:58 +0400

    Revised Statement defs and funcs

---
+++ c_compiler/include/ast/statement.cpp
@@ -0,0 +1,398 @@
+#include "statement.hpp"
+#include "context.hpp"
+
+
+/*
+  GETTERS
+ */
+
+std::string CompoundStatement::getNodeType() const { 
+if (parser)
+  return "Scope";
+return "CompoundStatement"; };
+
+std::vector<const baseNode *> CompoundStatement::getChildren() const {
+  return {decl, stat};
+}
+
+/*
+  END OF GETTERS
+ */
+
+/*
+   PRINTERS (ASSEMBLY)
+ */
+
+Context CompoundStatement::generate_assembly(Context ctxt, int d) const {
+
+  ctxt = decl->generate_assembly(ctxt);
+
+  for (auto it: stat->getChildren()){
+    it->generate_assembly(ctxt,d);
+  }
+  return ctxt;
+}
+
+
+Context ExprStatement::generate_assembly(Context ctxt, int d) const {
+  expr->generate_assembly(ctxt,d);
+  return ctxt;
+}
+
+
+void ExprStatement::python_print(std::ostream& stream) const {
+if (!parser)
+  stream << "Expr Statement NOT YET IMPLEMENTED"                    << std::endl;
+}
+
+void ConditionalStatement::python_print(std::ostream &stream) const {
+  stream << "Conditional Statement NOT YET IMPLEMENTED"             << std::endl;
+}
+
+void CompoundStatement::python_print(std::ostream &stream) const {
+  stream << "Compound Statement NOT YET IMPLEMENTED"                << std::endl;
+}
+
+
+const Expression *SelectionStatement::getCondition() const              { return getConditions()[0]; };
+
+std::vector<const Expression *>SelectionStatement::getConditions() const {
+  return {cond};
+}
+
+std::string IfStatement::getNodeType() const {
+  if (parser)
+    return "Scope";
+  return "IfStatement";
+}
+
+std::string IfElseStatement::getNodeType() const {
+  if (parser)
+    return "Scope";
+  return "IfElseStatement";
+}
+
+std::string SwitchStatement::getNodeType() const {
+  return "SwitchStatement";
+}
+
+
+
+/*
+  END OF GETTERS
+ */
+
+
+ /*
+   PRINT (ASSEMBLY)
+  */
+
+Context SwitchStatement::generate_assembly(Context ctxt, int d) const {
+  //using a temp context
+  Context temp_ctxt = ctxt;
+  
+  std::string EndLabel = "switch_end_label" + genUniqueID();
+  temp_ctxt.setBreak(EndLabel);
+  
+  cond->generate_assembly(temp_ctxt);
+  // Print stat
+  getBody()->generate_assembly(temp_ctxt);
+  // break goto
+  ctxt.ss() << EndLabel << ":"                                      << std::endl; 
+  ctxt.ss() << "\tnop"                                              << std::endl;
+  
+  return ctxt;
+};
+
+Context IfElseStatement::generate_assembly(Context ctxt, int d) const {
+  ctxt.ss() << "## " << getNodeType()                               << std::endl;
+
+  std::string _ID_ = genUniqueID();
+  std::string end = "$ifend" + _ID_;
+  std::string start = "$ifstart" + _ID_;
+  
+  getCondition()->generate_assembly(ctxt);
+
+  ctxt.ss() << "\tbne\t$2,$0," << start                             << std::endl;
+  ctxt.ss() << "\tnop"                                              << std::endl;
+    // exe stat2 and jump
+  ctxt = stat2->generate_assembly(ctxt);
+  ctxt.ss() << "\tb\t" << end                                       << std::endl;
+  ctxt.ss() << "\tnop"                                              << std::endl;
+
+  // exe stat1
+  ctxt.ss() << start << ":"                                         << std::endl;
+  ctxt = stat1->generate_assembly(ctxt);
+  ctxt.ss() << end << ":"                                           << std::endl;
+  return ctxt;
+}
+
+
+/*
+  PRINT PYTHONN
+ */
+
+void IfStatement::python_print(std::ostream &stream) const {
+    stream << "IfStatement NOT YET IMPLEMENTED"                     << std::endl;
+}
+
+void IfElseStatement::python_print(std::ostream &stream) const {
+    stream << "IfElseStatement NOT YET IMPLEMENTED"                 << std::endl;
+}
+/*
+  END OF PRINTERS
+ */
+
+
+/*
+  GETTERS
+ */
+std::string JumpStatement::getNodeType() const                                      { return "JumpStatement";       }
+std::string GotoStatement::getNodeType() const                                      { return "GotoStatement";       }
+std::string ContinueStatement::getNodeType() const                                  { return "ContinueStatement";   }
+std::string BreakStatement::getNodeType() const                                     { return "BreakStatement";      }
+std::string ReturnStatement::getNodeType() const                                    { return "ReturnStatement";     }   
+
+std::vector<const baseNode *> ReturnStatement::getChildren() const                  { return {expr}; }
+
+/*
+  END OF GETTERS
+ */
+
+/*
+  PRINT ASSEMBLY
+ */
+
+Context ReturnStatement::generate_assembly(Context ctxt, int d) const{
+  ctxt = expr->generate_assembly(ctxt);
+  ctxt.ss() << "\tj\t" << ctxt.getF() << " # end of current func"   << std::endl;
+  ctxt.ss() << "\tnop"                                              << std::endl;
+  return ctxt;
+}
+
+Context BreakStatement::generate_assembly(Context ctxt, int d) const{
+  
+  ctxt.ss() << "\tj\t" << ctxt.getBreak() << " # end of current loop"      << std::endl;
+  ctxt.ss() << "\tnop"                                              << std::endl;
+  return ctxt;
+}
+
+Context ContinueStatement::generate_assembly(Context ctxt, int d) const{
+  
+  ctxt.ss() << "\tj\t" << ctxt.getContinue()<< " # beginning of current loop"<< std::endl;
+  ctxt.ss() << "\tnop"                                              << std::endl;
+  return ctxt;
+}
+
+Context GotoStatement::generate_assembly(Context ctxt, int d) const{
+  
+  ctxt.ss() << "\tj\t" << label << "# jump to label"                << std::endl;
+  ctxt.ss() << "\tnop"                                              << std::endl;
+  return ctxt;
+}
+
+
+/*
+  PRINT PYTHONN
+ */
+
+void JumpStatement::python_print(std::ostream& stream) const {
+    stream << "JUMPStatement NOT YET IMPLEMENTED"                   << std::endl;
+}
+
+
+/*
+  GETTERS
+ */
+std::string LabeledStatement::getNodeType() const                                   { return "LabeledStatement"; }
+std::string Case::getNodeType() const                                               { return "Case"; }
+std::string Default::getNodeType() const                                            { return "Default"; }
+
+/*
+  END OF GETTERS
+ */
+
+/*
+  PRINT ASSEMBLY
+ */
+
+// Return statement has an expression (or not expression if void function)
+Context LabeledStatement::generate_assembly(Context ctxt, int d) const{
+  ctxt.ss() << id << ": # label before labelled statement"          << std::endl;
+  
+  ctxt = stat->generate_assembly(ctxt);
+  return ctxt;
+}
+
+
+Context Case::generate_assembly(Context ctxt, int d) const {
+
+  std::string caseLabel = "case" + genUniqueID();
+  // Evaluate expression.
+  ctxt.push(2);
+  ctxt = expr->generate_assembly(ctxt,3);
+
+  ctxt.pop(2);
+  
+  // $2!=$3? goto caseLabel
+  ctxt.ss() << "\tbne\t$2,$3," << caseLabel                         << std::endl;
+  ctxt = stat->generate_assembly(ctxt);
+
+  // if expression is not equal to 
+  ctxt.ss() << caseLabel << ": # unique case label just in case"    << std::endl;
+  
+  return ctxt;
+}
+
+Context DefaultLabel::generate_assembly(Context ctxt, int d) const {
+  std::string dLabel = "default" + genUniqueID();
+
+  ctxt.ss() << dLabel << ": # unique default label just in case"    << std::endl;
+  ctxt = stat->generate_assembly(ctxt);
+
+  return ctxt;
+}
+
+
+
+
+std::string Iteration::getNodeType() const { 
+  if (parser)
+    return "Scope";
+  return "Iteration"; }
+std::string ForStatement::getNodeType() const { 
+  if (parser)
+    return "Scope";
+  return "ForStatement"; }
+std::string WhileStatement::getNodeType() const { 
+  if (parser)
+    return "Scope";
+  return "WhileStatement"; }
+std::string DoWhileStatement::getNodeType() const { 
+  if (parser)    
+    return "Scope";
+  return "DoWhileStatement"; }
+
+
+std::string ExprExprFor::getNodeType() const                        { return "EEForStatement"; }
+std::string DecExprFor::getNodeType() const                         { return "DEForStatement"; }
+std::string ExprExprExprFor::getNodeType() const                    { return "EEEForStatement"; }
+std::string DecExprExprFor::getNodeType() const                     { return "DEEForStatement"; }
+
+
+
+/*
+   GETTERS
+ */
+
+std::vector<const baseNode*> WhileStatement::getChildren() const {
+  return {cond,stat1};
+}
+
+std::vector<const baseNode*> ForStatement::getChildren() const {
+  return {cond1,stat1};
+}
+
+std::vector<const baseNode*> ExprExprFor::getChildren() const {
+  return {cond1,cond2,stat1};
+}
+
+std::vector<const baseNode*> ExprExprExprFor::getChildren() const {
+  return {cond1,cond2,cond3,stat1};
+}
+
+std::vector<const baseNode*> DecExprFor::getChildren() const {
+  return {dec,cond1,stat1};
+}
+
+std::vector<const baseNode*> DecExprExprFor::getChildren() const {
+  return {dec,cond1,cond2,stat1};
+}
+
+/*
+   END OF GETTERS
+ */
+
+/*
+   PRINTERS
+ */
+
+Context print_while(Context ctxt, const Iteration * in, const Statement * stat1){
+  // Create labels
+  std::string _ID_ = genUniqueID();
+  std::string cond       = "$cond"       + _ID_;
+  std::string whilestart = "$whilestart" + _ID_;
+  std::string whilend    = "$whilend"    + _ID_;
+  
+  std::string type = in->getNodeType();
+  if (type=="ExprExprFor" || type=="ExprExprExprFor"){
+    ctxt = in->getChildren().at(0)->generate_assembly(ctxt,2);
+  }
+
+  //Using a temp context
+  Context temp_ctxt = ctxt;
+  if (type=="DecExprFor" || type=="DecExprExprFor"){
+    temp_ctxt = in->getChildren().at(0)->generate_assembly(temp_ctxt,2);
+  }
+  
+  temp_ctxt.setBreak(whilend);
+  temp_ctxt.setContinue(cond);
+  
+  if (type=="DoWhileStatement"){
+    // Loop once 
+    ctxt.ss() << "\tb\t" << whilestart                              << std::endl;
+  }
+  // label for condition
+  ctxt.ss() << cond << ":"                                          << std::endl;
+  if (type=="ExprExprExprFor" || type=="DecExprExprFor"){
+    in->getConditions().at(1)->generate_assembly(temp_ctxt,2);
+  }
+  in->getConditions().at(0)->generate_assembly(temp_ctxt,2);
+  // If cond=0, goto whilend
+  ctxt.ss() << "\tbeq\t$2,$0," << whilend                           << std::endl;
+  ctxt.ss() << "\tnop"                                              << std::endl;
+  // exe statment
+  ctxt.ss() << whilestart << ":"                                    << std::endl;
+  stat1->generate_assembly(temp_ctxt);
+
+  ctxt.ss() << "\tb\t" << cond                                      << std::endl;
+  ctxt.ss() << "\tnop"                                              << std::endl;
+  ctxt.ss() << whilend << ":"                                       << std::endl;
+
+  return ctxt;
+}
+
+Context Iteration::generate_assembly(Context ctxt, int d) const {
+  print_while(ctxt, this, stat1);
+
+  return ctxt;
+}
+
+
+Context WhileStatement::generate_assembly(Context ctxt, int d) const {
+  print_while(ctxt, this, stat1);
+
+  return ctxt;
+}
+
+Context DoWhileStatement::generate_assembly(Context ctxt, int d) const {
+  print_while(ctxt, this, stat1);
+
+  return ctxt;
+}
+
+Context ForStatement::generate_assembly(Context ctxt, int d) const {
+  
+  return print_while(ctxt, this, stat1);
+}
+
+Context DecExprExprFor::generate_assembly(Context ctxt, int d) const {
+  return print_while(ctxt, this, stat1);
+}
+
+
+
+/*
+   END OF PRINTERS
+ */
+
---
+++ c_compiler/include/ast/statement.hpp
@@ -0,0 +1,341 @@
+#ifndef STATEMENT_HPP
+#define STATEMENT_HPP
+
+#include "baseNode.hpp"
+#include "base_expr.hpp"
+#include "declaration.hpp"
+
+
+class ExprStatement : public Statement {
+private:
+  const Expression *expr;
+public:
+  ExprStatement(const Expression *_expr) : expr(_expr){};
+  ~ExprStatement(){};
+
+  virtual std::string getNodeType() const override { return "ExprStatement"; };
+  virtual std::vector<const baseNode *> getChildren() const override { return {expr}; }
+  virtual Context generate_assembly(Context ctxt, int d = 2) const override;
+  virtual void python_print(std::ostream& stream) const override;
+};
+
+/*
+   Condition is type Expression
+   Result is type Statement
+*/
+class ConditionalStatement : public Statement {
+protected:
+  const Statement *stat1;
+
+public:
+  virtual ~ConditionalStatement() {
+    delete stat1;
+  };
+
+  ConditionalStatement(const Statement *_stat1) : stat1(_stat1) {};
+
+  virtual std::string getNodeType() const { return "ConditionalStatement"; };
+  virtual void python_print(std::ostream &stream) const;
+
+  virtual std::vector<const baseNode *> getChildren() const override { return {stat1}; }
+  virtual std::vector<const Expression *> getConditions() const = 0;
+  virtual const Statement *getBody() const { return stat1; };
+};
+
+/*
+ x no of DecLs, x no of Stats
+*/
+class CompoundStatement : public Statement {
+private:
+  const List *decl;
+  const List *stat;
+
+public:
+  virtual ~CompoundStatement() {
+    delete decl;
+    delete stat;
+  };
+  CompoundStatement() : decl(new List({}) ), stat(new List({}) ) {};
+  CompoundStatement(const List *_decl, const List *_stat) : decl(_decl), stat(_stat) {};
+
+  virtual std::string getNodeType() const override;
+  virtual std::vector<const baseNode *> getChildren() const override;
+  virtual void python_print(std::ostream &stream) const override;
+  virtual Context generate_assembly(Context ctxt, int d = 2) const override;
+};
+
+
+class SelectionStatement : public ConditionalStatement {
+protected:
+  const Expression *cond;
+public:
+  SelectionStatement(const Expression *_cond, const Statement *_stat)
+      : ConditionalStatement(_stat), cond(_cond) {};
+  
+  virtual std::string getNodeType() const { return "SelectionStatement"; };
+  virtual const Expression *getCondition() const;
+  virtual std::vector<const Expression *> getConditions() const;
+};
+
+/*
+    If cond is true, will exe stat
+*/
+class IfStatement : public SelectionStatement {
+public:
+  virtual ~IfStatement(){};
+  virtual std::string getNodeType() const override;
+  IfStatement(const Expression *_cond, const Statement *_stat)
+      : SelectionStatement(_cond, _stat) {};
+
+  virtual void python_print(std::ostream &stream) const;
+};
+
+/*
+    If cond true, exe stat1, if cond false, exe stat2
+*/
+class IfElseStatement : public IfStatement {
+private:
+  const Statement *stat2;
+
+public:
+  IfElseStatement(const Expression *_cond, const Statement *_stat1, const Statement *_stat2)
+      : IfStatement(_cond, _stat1), stat2(_stat2) {};
+
+  virtual std::string getNodeType() const override;
+  virtual Context generate_assembly(Context ctxt, int d = 2) const override;
+  virtual std::vector<const baseNode *> getChildren() const override { return {stat1,stat2}; }
+  virtual void python_print(std::ostream &stream) const;
+};
+
+
+
+class SwitchStatement : public SelectionStatement {
+public:
+  SwitchStatement(const Expression *_cond, const Statement *_stat)
+      : SelectionStatement(_cond, _stat) {};
+
+  virtual std::string getNodeType() const override;
+  virtual Context generate_assembly(Context ctxt, int d = 2) const override;
+};
+
+
+/*
+    Unconditional jump to a label
+ */
+class JumpStatement : public Statement {
+public:
+  JumpStatement(){};
+  virtual ~JumpStatement(){};
+
+  virtual std::string getNodeType() const override;
+  virtual void python_print(std::ostream& stream) const override;
+};
+
+
+class GotoStatement : public JumpStatement {
+private:
+  std::string targetLabel;
+
+public:
+  GotoStatement(const std::string label) : TargetLabel(label){};
+  virtual ~GotoStatement(){};
+  
+  virtual std::string getNodeType() const override;
+  virtual Context generate_assembly(Context ctxt, int d = 2) const override;
+};
+
+
+class ContinueStatement : public JumpStatement {
+public:
+  ContinueStatement(){};
+  virtual ~ContinueStatement(){};
+
+  virtual std::string getNodeType() const override;
+  virtual Context generate_assembly(Context ctxt, int d = 2) const override;
+};
+
+
+
+class BreakStatement : public JumpStatement {
+public:
+  BreakStatement(){};
+  virtual ~BreakStatement(){};
+  
+  virtual std::string getNodeType() const override;
+  virtual Context generate_assembly(Context ctxt, int d = 2) const override;
+};
+
+/*!
+    Requires an expression
+ */
+class ReturnStatement : public JumpStatement {
+private:
+  const ExprStatement *expr;
+
+public:
+  ReturnStatement(const ExprStatement *_expr) : expr(_expr){};
+
+  virtual std::string getNodeType() const override;
+  virtual std::vector<const baseNode *> getChildren() const override;
+  virtual Context generate_assembly(Context ctxt, int d = 2) const override;
+};
+
+class LabeledStatement : public Statement {
+private:
+  std::string id;
+  const Statement *stat;
+public:
+  virtual ~LabeledStatement(){};
+  LabeledStatement(std::string *_id, const Statement *_stat) : stat(_stat), id(*_id) {};
+
+  std::string getNodeType() const override;
+  virtual Context generate_assembly(Context ctxt, int d = 2) const override;
+};
+
+class Case : public Statement {
+private:
+  const Expression *expr;
+  const Statement *stat;
+
+public:
+  virtual ~Case(){};
+  Case(const Expression *_expr, const Statement *_stat) : expr(_expr), stat(_stat){};
+
+  std::string getNodeType() const override;    
+  virtual Context generate_assembly(Context ctxt, int d = 2) const override;
+};
+
+
+class Default : public Statement {
+private: 
+  const Statement *stat;
+public:
+  virtual ~Default(){};
+  Default(const Statement *_stat) : stat(_stat) {};
+  
+  virtual std::string getNodeType() const override { return "Default";};
+  virtual Context generate_assembly(Context ctxt, int d = 2) const override;
+};
+
+
+
+class Iteration : public ConditionalStatement {
+public:
+  virtual ~Iteration(){};
+  virtual std::string getNodeType() const;
+  // virtual std::vector<const baseNode *> getChildren() const;
+  // virtual std::vector<const Expression*> getConditions() const = 0;
+
+  Iteration(const Statement *_stat) : ConditionalStatement(_stat) {}
+  virtual std::vector<const baseNode *> getChildren() const = 0;
+  virtual Context generate_assembly(Context ctxt, int d = 2) const override;
+};
+
+
+/*!
+    exe stat as long as cond (type expression) is true
+ */
+class WhileStatement : public Iteration {
+private:
+  const Expression *cond;
+public:
+  virtual ~WhileStatement(){};
+  
+  virtual std::string getNodeType() const override;
+  virtual std::vector<const Expression*> getConditions() const override { return {cond}; };
+
+  WhileStatement(const Expression *_cond, const Statement *_stat)
+      : Iteration(_stat), cond(_cond) {};
+  virtual std::vector<const baseNode *> getChildren() const override;
+  //virtual Context generate_assembly(Context ctxt, int d = 2) const override;
+};
+
+
+/*
+    exe stat  while ( cond(type expression) = true )
+ */
+class DoWhileStatement : public WhileStatement {
+public:
+  virtual ~DoWhileStatement(){};
+  virtual std::string getNodeType() const override;
+
+  DoWhileStatement(const Expression *_cond, const Statement *_stat) : WhileStatement(_cond, _stat) {};
+  //virtual Context generate_assembly(Context ctxt, int d = 2) const override;
+};
+
+
+
+class ForStatementBase : public Iteration {
+protected:
+  const ExprStatement *cond1;
+public:
+  virtual std::string getNodeType() const override;
+  ForStatementBase(const ExprStatement *c1, const Statement *s1)
+      : IterationStatement(s1), cond1(c1)  {};
+  virtual std::vector<const Expression*> getConditions() const override {
+    return {(const Expression*)cond1};
+  };
+  virtual std::vector<const baseNode *> getChildren() const override;
+  // virtual Context generate_assembly(Context ctxt, int d = 2) const override;
+};
+
+class ExprExprFor : public ForStatementBase {
+protected:
+  const ExprStatement *cond2;
+public:
+  virtual std::string getNodeType() const override;
+  ExprExprFor(const ExprStatement *_cond1, const ExprStatement *_cond2, const Statement *_stat1)
+      : ForStatementBase(_cond1, _stat1), cond2(_cond2) {};
+  
+  virtual std::vector<const Expression*> getConditions() const override {
+    return {(const Expression*)cond2, (const Expression*)cond1 };
+  };
+  virtual std::vector<const baseNode *> getChildren() const override;
+};
+
+class ExprExprExprFor : public ExprExprFor {
+private:
+  const Expression *cond3;
+public:
+  virtual std::string getNodeType() const override;
+  ExprExprExprFor(const ExprStatement *_cond1, const ExprStatement *_cond2, const Expression *_cond3, const Statement *_stat1)
+      : ExprExprFor(_cond1, _cond2, _stat1), cond3(_cond3)  {};
+  
+  virtual std::vector<const Expression*> getConditions() const override {
+    return {(const Expression*)cond2, cond3, (const Expression*)cond1 };
+  };
+  virtual std::vector<const baseNode *> getChildren() const override;
+};
+
+class DecExprFor : public ForStatementBase {
+protected:
+  const Declaration *decl;
+public:
+  virtual std::string getNodeType() const override;
+  DecExprFor(const Declaration *_decl, const ExprStatement *_cond1, const Statement *_stat1)
+      :  ForStatementBase(_cond1, _stat1), decl(_decl)  {};
+  
+  virtual std::vector<const Expression*> getConditions() const override {
+    return {(const Expression*)cond1};
+  };
+  virtual std::vector<const baseNode *> getChildren() const override;
+  //virtual void python_print(std::ostream& stream) const override;
+};
+
+class DecExprExprFor : public DecExprFor {
+private:
+  const Expression *cond2;
+public:
+  virtual std::string getNodeType() const override;
+  DecExprExprFor(const Declaration *_decl, const ExprStatement *_cond1, const Expression *_cond2, const Statement *_stat1)
+      : DecExprFor(_decl, _cond1, _stat1), cond2(_cond2)  {};
+
+  virtual std::vector<const Expression*> getConditions() const override {
+    return {(const Expression*)cond1, cond2};
+  };
+  virtual std::vector<const baseNode *> getChildren() const override;
+  // virtual Context generate_assembly(Context ctxt, int d = 2) const override;
+};
+
+
+#endif
